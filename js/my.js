4b6c59cc770e6ca544e04faa937e68ebc3d3b280
AlameenAzad <alameen.azad@protonmail.com>
2019-11-18 14:51:21 +0300

    design changes

--- js/my.js
+++ js/my.js
@@ -1,7 +1,10 @@
-$(window).on('load', function() {
- // code here
-
-    setTimeout(function(){$('.loader').slideUp( "slow", function(){} );}, 3000);
+$(window).on('load', function () {
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+    setTimeout(function () {
+        $('.loader').slideUp("slow", function () {});
+    }, 2000);
 });
 $(function () {
 
@@ -10,9 +13,9 @@ $(function () {
     $('.navbar-nav>li>a').on('click', function () {
         $('.navbar-collapse').collapse('hide');
     });
-    scrollTo('.projects','#toProjects');
-    scrollTo('.contact','#toContact');
-    scrollTo('.home','#toHome');
+    scrollTo('.projects', '#toProjects');
+    scrollTo('.contact', '#toContact');
+    scrollTo('.home', '#toHome');
 
     function scrollTo(from, to) {
         $(from).click(function () {
@@ -21,138 +24,7 @@ $(function () {
             }, 2000);
         });
     }
-    //    /*
-    //
-    //ThreeJs custom waves
-    //Original script by ThreeJS : https://threejs.org/examples/canvas_particles_waves.html
-    //Modified version for Cloudoru by Kevin Rajaram : http://kevinrajaram.com
-    //Date: 08/14/2014
-    //
-    //
-    //*/
-    //
-    //    var SEPARATION = 50,
-    //        AMOUNTX = 60,
-    //        AMOUNTY = 30;
-    //
-    //    var container;
-    //    var camera, scene, renderer;
-    //    /*
-    //
-    //    if (window.WebGLRenderingContext){
-    //    	renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
-    //    	}
-    //    else {
-    //    	renderer = new THREE.CanvasRenderer();
-    //    	}
-    //    */
-    //
-    //    var particles, particle, count = 0;
-    //
-    //    var windowHalfX = window.innerWidth / 2;
-    //    var windowHalfY = window.innerHeight / 2;
-    //
-    //    init();
-    //    animate();
-    //
-    //    function init() {
-    //
-    //        container = document.createElement('div');
-    //        document.body.appendChild(container);
-    //        if (container) {
-    //            container.className += container.className ? ' waves' : 'waves';
-    //        }
-    //
-    //        camera = new THREE.PerspectiveCamera(120, window.innerWidth / window.innerHeight, 1, 10000);
-    //        camera.position.y = 180; //changes how far back you can see i.e the particles towards horizon
-    //        camera.position.z = 10; //This is how close or far the particles are seen
-    //
-    //        camera.rotation.x = 0.35;
-    //
-    //        scene = new THREE.Scene();
-    //
-    //        particles = new Array();
-    //
-    //        var PI2 = Math.PI * 2;
-    //        var material = new THREE.SpriteCanvasMaterial({
-    //
-    //            color: 0xE59500, //changes color of particles
-    //            program: function (context) {
-    //
-    //                context.beginPath();
-    //                context.arc(0, 0, 0.5, 0, PI2, true);
-    //                context.fill();
-    //
-    //            }
-    //
-    //        });
-    //
-    //        var i = 0;
-    //
-    //        for (var ix = 0; ix < AMOUNTX; ix++) {
-    //
-    //            for (var iy = 0; iy < AMOUNTY; iy++) {
-    //
-    //                particle = particles[i++] = new THREE.Sprite(material);
-    //                particle.position.x = ix * SEPARATION - ((AMOUNTX * SEPARATION) / 2);
-    //                particle.position.z = iy * SEPARATION - ((AMOUNTY * SEPARATION) - 10);
-    //                scene.add(particle);
-    //
-    //            }
-    //
-    //        }
-    //
-    //        renderer = new THREE.CanvasRenderer();
-    //        renderer.setSize(window.innerWidth, window.innerHeight);
-    ////        renderer.setClearColor(, 1);
-    //        container.appendChild(renderer.domElement);
-    //
-    //        window.addEventListener('resize', onWindowResize, false);
-    //
-    //    }
-    //
-    //    function onWindowResize() {
-    //
-    //        windowHalfX = window.innerWidth / 2;
-    //        windowHalfY = window.innerHeight / 2;
-    //
-    //        camera.aspect = window.innerWidth / window.innerHeight;
-    //        camera.updateProjectionMatrix();
-    //
-    //        renderer.setSize(window.innerWidth, window.innerHeight);
-    //
-    //    }
-    //
-    //    function animate() {
-    //
-    //        requestAnimationFrame(animate);
-    //
-    //        render();
-    //
-    //    }
-    //
-    //    function render() {
-    //
-    //        var i = 0;
-    //
-    //        for (var ix = 0; ix < AMOUNTX; ix++) {
-    //
-    //            for (var iy = 0; iy < AMOUNTY; iy++) {
-    //
-    //                particle = particles[i++];
-    //                particle.position.y = (Math.sin((ix + count) * 0.5) * 15) + (Math.sin((iy + count) * 0.5) * 15);
-    //                particle.scale.x = particle.scale.y = (Math.sin((ix + count) * 0.5) + 2) * 4 + (Math.sin((iy + count) * 0.5) + 1) * 4;
-    //
-    //            }
-    //
-    //        }
-    //
-    //        renderer.render(scene, camera);
-    //
-    //        // This increases or decreases speed
-    //        count += 0.05;
-    //
-    //    }
+
     var particles = document.getElementById("particles");
     var border = ["30%", "60%", "10%", "50%", "80%"];
     var colors = ["#FF6B6B", "#FFE66D", "#4472CA"];
@@ -212,10 +84,56 @@ $(function () {
 
     function main(event) {
         getParticles();
-        //    particles.addEventListener("click", createParticle);
     }
 
     window.addEventListener("resize", main);
     window.addEventListener("load", main);
+
+    $('.player-card').mousemove(function (e) {
+        $(this).children('div').children('div').children('.content').parallax(50, e);
+        //        $('img').parallax(70, e);
+    });
+
+    // Parallax mouse
+    $.fn.parallax = function (resistance, mouse) {
+        $el = $(this);
+        TweenLite.to($el, 0.2, {
+            x: -((mouse.clientX - (window.innerWidth / 2)) / resistance),
+            y: -((mouse.clientY - (window.innerHeight / 2)) / resistance)
+        });
+    };
+
+    animations();
+    $(window).resize(function () {
+        animations();
+    });
+    var widthJs = 1024;
+
+    function animations() {
+
+        $('.player-card').on('mousemove', function (e) {
+
+            if ($(window).width() > widthJs) {
+
+                var $this = $(this);
+
+                var x = (event.clientX / $(window).width()) - 0.5;
+                var y = (event.clientY / $(window).height()) - 0.5;
+
+                TweenLite.to($this, 0.6, {
+                    rotationY: 10 * x,
+                    rotationX: 5 * y,
+                    ease: Power1.easeOut,
+                    transformPerspective: 500,
+                    transformOrigin: "center"
+                });
+
+            }
+
+        });
+    }
+
+
+
 
 });
